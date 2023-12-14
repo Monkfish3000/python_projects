9760cf3ddea488f018e93ce51fcae56d25aeb464
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-12-14 16:52:22 +0100

    added options and 'off' ability

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -24,12 +24,21 @@ MENU = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-while True:
-    choice = input("What would you like? (espresso/latte/cappuccino: ")
+is_on = True
 
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: {profit}")
