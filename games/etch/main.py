aa5020fb13e1ed86b23ca4fa52614a016516e599
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2024-01-12 13:55:50 +0100

    basic etcha sketch game built

--- games/etch/main.py
+++ games/etch/main.py
@@ -6,7 +6,20 @@ screen = Screen()
 def move_fowards():
     monk.forward(10)
 
+def move_backwards():
+    monk.backward(10)
+
+def turn_right():
+    monk.right(90)
+
+def turn_left():
+    monk.left(90)
 
 screen.listen()
-screen.onkey(move_fowards, 'space')
+
+screen.onkey(move_fowards, 'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(turn_left, 'a')
+screen.onkey(turn_right, 'd')
+
 screen.exitonclick()
